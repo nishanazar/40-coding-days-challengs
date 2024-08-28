77786d78ebe552badded0314580c135269997940
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-08-28 11:27:24 +0500

    day1 completed

--- day1/Q1.js
+++ day1/Q1.js
@@ -1,14 +1,11 @@
+"use strict";
 //--------------Question: 1-----------------------
-
-function mergeTextWithNumber(text: string, num: number) {
-  return `${text}: ${num}`;
+function mergeTextWithNumber(text, num) {
+    return `${text}: ${num}`;
 }
-
 console.log(mergeTextWithNumber("Age", 22));
-
 //--------------Question: 2-----------------------
-
-function remainder(num1: number, num2: number) {
-  return num1 % num2;
+function remainder(num1, num2) {
+    return num1 % num2;
 }
 console.log(remainder(5, 2));
