dacf4ecbcb8bdecf162df4cded9e7b8364c51fa1
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-09-07 12:05:52 +0500

    Day17 completed

---
+++ day17/Q17.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//---------------Question: 33---------------------------
+// Generate a random integer between 1 and 10.
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+//---------------Question: 34---------------------------
+let num1 = -5;
+let num2 = 5;
+let difference = Math.abs(num1 - num2);
+console.log(difference);
---
+++ day17/Q17.ts
@@ -0,0 +1,13 @@
+//---------------Question: 33---------------------------
+// Generate a random integer between 1 and 10.
+let random = Math.floor(Math.random() *10) + 1
+console.log(random);
+
+
+//---------------Question: 34---------------------------
+
+
+let num1 = -5;
+let num2 = 5;
+let difference = Math.abs(num1 - num2);
+console.log(difference);  //10
