58e07a25fff573b9bf6ab64232873106c4d270fc
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-09-16 15:04:17 +0500

    day25 complete

---
+++ day25/Q25.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//----------Question: 49------------------------
+let expression = "value";
+switch (expression) {
+    case "value1":
+        console.log("Case 1 matched");
+        break;
+    case "value2":
+        console.log("Case 1 matched");
+        break;
+    default:
+        console.log("'No match, this is the default case");
+}
+//----------Question: 50------------------------
+// Write a loop that logs numbers from 1 to 10 to the console.
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
---
+++ day25/Q25.ts
@@ -0,0 +1,19 @@
+//----------Question: 49------------------------
+let expression = "value";
+
+switch (expression) {
+  case "value1":
+    console.log("Case 1 matched");
+    break;
+  case "value2":
+    console.log("Case 1 matched");
+    break;
+  default:
+    console.log("'No match, this is the default case");
+}
+
+//----------Question: 50------------------------
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
