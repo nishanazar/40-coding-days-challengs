70dbea47c236e2f3370a69e5df7aaf961d50d761
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-09-17 10:37:49 +0500

    day26 complete

---
+++ day26/Q26.js
@@ -0,0 +1,12 @@
+"use strict";
+//----------Question: 51------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+for (let i = 1; i <= 5; i++) {
+    console.log(`Hello World! ${i}`);
+}
+//----------Question: 52------------------------
+// Use a for...of loop to iterate through an array of your favorite movies and log each one.
+let movie = ["ferozen", "Tere bin", "south", "bollyword"];
+for (const vale of movie) {
+    console.log(`My favourite movie ${vale}`);
+}
---
+++ day26/Q26.ts
@@ -0,0 +1,13 @@
+//----------Question: 51------------------------
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Hello World! ${i}`);
+}
+
+//----------Question: 52------------------------
+
+let movie: string[] = ["ferozen", "Tere bin", "south", "bollyword"];
+
+for (const vale of movie) {
+  console.log(`My favourite movie ${vale}`);
+}
